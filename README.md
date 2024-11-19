a9f135d037ecaefcb1268f1e8d2fa899b234b0eb
Wonhyuk Choi <whchoi000@gmail.com>
2024-11-19 15:07:29 +0900

    Initial project

---
+++ README.md
@@ -0,0 +1,3 @@
+# cross_mate
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/cross_mate/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cross_mate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
